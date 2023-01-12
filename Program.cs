7eb43d985cfdca272c28fb806447185f0318dd0a
Ngan Tran <trann0709@gmail.com>
2023-01-12 11:35:28 -0600

    setting up app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicGroceryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello FinnBob!");
+        }
+    }
+}
